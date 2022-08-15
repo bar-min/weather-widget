a10218cf555bcd8ad51a5b7aead0e17dbe45a4fe
Kel Mahum <yamatanokingtheninja@gmail.com>
2022-08-15 05:07:14 +0400

    update config 1.0.0

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    extract: false
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+        ? '/'
+        : '/weather/'
 })
